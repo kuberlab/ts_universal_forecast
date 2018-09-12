3f743401655182b55b99e278e08d685f9bd388b5
Alexander Gunin <guninalexander@gmail.com>
2018-09-12 22:19:59 +0300

    submipt results

--- train.py
+++ train.py
@@ -215,7 +215,7 @@ def test(checkpoint_dir, checkpoint_path, params):
     for p in predictions:
         pid = ids[j]
         for i in range(len(pid)):
-            value.append(int(p[i]))
+            value.append(int(round(p[i][0])))
             id.append(pid[i])
         j+=1
     logging.info('values: {}'.format(value))
