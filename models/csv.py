a48a241b2d67479e2bde06d4aa6dd8e236349774
Alexander Gunin <guninalexander@gmail.com>
2018-09-14 13:31:20 +0300

    try more simple

--- models/csv.py
+++ models/csv.py
@@ -394,6 +394,7 @@ def encoder_model_fn(features, y_variables, mode, params=None, config=None):
         #denominator = tf.where(tf.equal(denominator, 0), tf.ones_like(denominator), denominator)
         smape = tf.abs(predictions - y_variables) / denominator
         loss_op = tf.losses.compute_weighted_loss(smape)
+        logging.info('Compute loss: ')
         smape = 200 * smape
         metrics['SMAPE'] = tf.metrics.mean(smape)
         if mode == tf.estimator.ModeKeys.TRAIN:
