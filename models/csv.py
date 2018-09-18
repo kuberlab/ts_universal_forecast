623a8080ca98bc7cf2b31d4a351c8b1cfe1ef451
Alexander Gunin <guninalexander@gmail.com>
2018-09-18 15:13:18 +0300

    try gru

--- models/csv.py
+++ models/csv.py
@@ -406,7 +406,7 @@ def encoder_model_fn(features, y_variables, mode, params=None, config=None):
     enc_cell = tf.contrib.rnn.GRUBlockCellV2(num_units=params['hidden_size'])
     dec_cell = tf.contrib.rnn.GRUBlockCellV2(num_units=params['hidden_size'])
     initial_state = enc_cell.zero_state(params['batch_size'], dtype=tf.float32)
-    _, encoder_state = tf.nn.dynamic_rnn(enc_cell, enc_output,initial_state=initial_state,dtype=tf.float32)
+    _, encoder_state = tf.nn.dynamic_rnn(enc_cell, enc_output,initial_state=initial_state,dtype=tf.float32,time_major=True)
 
     def cond_fn(time, prev_output, prev_state, targets):
         return time < params['output_window_size']
